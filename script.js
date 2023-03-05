574b2a4e3e30f6e43b6e8b3b5baba8c42f9d2b90
Lush <lushhollis@gmail.com>
2023-03-04 21:21:17 -0500

    Add showAddBookPopup() to script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let authorInput=document.getElementById("author");
 let readInput=document.getElementById("read");
 let pagesInput=document.getElementById("pages");
 let readPagesInput=document.getElementById("readPages");
+let addBookPopup=document.getElementById("addBookPopup");
 
 function Book(title,author,read,pages,readPages){
     this.title=title;
@@ -11,25 +12,32 @@ function Book(title,author,read,pages,readPages){
     if (read!==undefined){
         if (read=="on" || read){
             this.read=true;
+            this.readPages=pages;
         }
         else if (read=="off"||read==false){
             this.read=false;
+            if (readPages!==undefined){
+                this.readPages=readPages;
+            }
         }
     }
     if (pages!==undefined){
         this.pages=pages;
-    }
-    if (read){
-        this.readPages=pages;
-    }
-    else if (readPages!==undefined){
-        this.readPages=readPages;
     }
 }
 
 function addBook(){
     library.push(new Book(titleInput.value,authorInput.value,readInput.value,pagesInput.value,readPagesInput.value));
     console.log(library[library.length-1]);
+    addBookPopup.style.display="none";
+    addBookPopup.style.width="1";
+    addBookPopup.style.height="1";
+}
+
+function showAddBookPopup(){
+    addBookPopup.style.display="block";
+    addBookPopup.style.width="40vw";
+    addBookPopup.style.height="96vh";
 }
 
 /*
